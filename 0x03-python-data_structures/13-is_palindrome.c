109c639b6e6f3a4078c57b94b7881a1f851ca7b2
Victory Omenihu <vicky4zee@gmail.com>
2022-08-29 01:01:18 -0700

    task 13

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,15 +1,60 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include "lists.h"
+#include <stdlib.h>
 
 /**
- * main - check the code for Holberton School students.
- * Return: Always 0.
+ * get_len - get the length of a linkedlist
+ * @head: head of the linkedlist
+ * Return: the length of a linkedlist
  */
 
-int main(void)
+int get_len(listint_t *head)
 {
-	listint_t *head;
+	listint_t *tmp = head;
+	int len = 0;
 
-	return (0);
+	while (tmp != NULL)
+	{
+		len++;
+		tmp = tmp->next;
+	}
+	return (len);
+}
+
+/**
+ * is_palindrome - check if a linkedlist is a palindrome
+ * @head: head of the linkedlist
+ * Return: 0 if it is not a palindrome, 1 if it is a palindrome
+ */
+
+int is_palindrome(listint_t **head)
+{
+	listint_t *tmp;
+	int *arr, len, i = 0, j = 0;
+
+	if (!head)
+		return (-1);
+	if (!(*head))
+		return (1);
+	len = get_len(*head);
+	arr = malloc(sizeof(*arr) * len);
+	tmp = *head;
+	while (tmp != NULL)
+	{
+		arr[i] = tmp->n;
+		i++;
+		tmp = tmp->next;
+	}
+	i -= 1;
+	while (i >= j)
+	{
+		if (arr[i] != arr[j])
+		{
+			free(arr);
+			return (0);
+		}
+		i--;
+		j++;
+	}
+	free(arr);
+	return (1);
 }
